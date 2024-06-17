92c7385868063dafd2f55f8cdf89433ab967ed0c
Lucas Tymen <lucas.tymen@gmail.com>
2024-06-17 20:59:35 +0200

    Configuring User profile

--- main/models.py
+++ main/models.py
@@ -33,3 +33,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author} on {self.article}'
+
+# Configuring User profile
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
