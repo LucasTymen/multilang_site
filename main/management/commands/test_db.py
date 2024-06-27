b899def7eb4a6f956bcc6707a9acdf02e6b7780b
Lucas Tymen <lucas.tymen@gmail.com>
2024-06-27 18:21:36 +0200

    fixed database access - articles and users are back

---
+++ main/management/commands/test_db.py
@@ -0,0 +1,13 @@
+# main/management/commands/test_db.py
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        db_conn = connections['default']
+        try:
+            c = db_conn.cursor()
+            self.stdout.write(self.style.SUCCESS('Database connection successful'))
+        except OperationalError:
+            self.stdout.write(self.style.ERROR('Database connection failed'))
